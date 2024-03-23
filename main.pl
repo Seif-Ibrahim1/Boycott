d98a3b865508d9373c4eca0ae3ee9655f5ff51fd
Shahd Mostafa <mostafashahd30@gmail.com>
2024-03-23 03:34:56 +0200

    Added point 4: getNumOfItems

--- main.pl
+++ main.pl
@@ -5,3 +5,10 @@
 getItemsInOrderById(CustName,OrderId,Items) :-
     customer(CustID,CustName),
     order(CustID,OrderId,Items).
+
+% 4
+getNumOfItems(CustName, OrderId, Count) :-
+    getItemsInOrderById(CustName, OrderId, Items),
+    length(Items, Count).
+    
+
