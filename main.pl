f71ae3f5f201ba0c77b83bf7dcb719c62b862e50
Shahd Mostafa <mostafashahd30@gmail.com>
2024-03-23 16:55:33 +0200

    Added point 7: whyToBoycott

--- main.pl
+++ main.pl
@@ -8,10 +8,16 @@ getItemsInOrderById(CustName,OrderId,Items) :-
 
 % 4
 getLength([], 0).
-getLength([Begin|End] , Count) :-
+getLength([_|End] , Count) :-
     getLength(End ,Length),
     Count is Length + 1.
 
 getNumOfItems(CustName, OrderId, Count) :-
     getItemsInOrderById(CustName, OrderId, Items),
     getLength(Items, Count).
+
+
+% 7
+whyToBoycott(Name, Justification) :-
+    item(Name, Company, _) -> boycott_company(Company, Justification)
+    ;boycott_company(Name, Justification).
