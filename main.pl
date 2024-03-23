3953333595b60ffd064efeddf53c0a8eafa9781b
maryamosama33 <maryamosama3383@gmail.com>
2024-03-23 16:48:15 +0200

    Add 5,6,9 and 10

--- main.pl
+++ main.pl
@@ -15,3 +15,48 @@ getLength([Begin|End] , Count) :-
 getNumOfItems(CustName, OrderId, Count) :-
     getItemsInOrderById(CustName, OrderId, Items),
     getLength(Items, Count).
+
+% 5
+calcPriceOfOrder(CustName, OrderId, TotalPrice) :-
+    getItemsInOrderById(CustName, OrderId, Items),
+    calcTotalPrice(Items, TotalPrice).
+
+calcTotalPrice([], 0).
+calcTotalPrice([Item|Rest], TotalPrice) :-
+    item(Item, _, Price),
+    calcTotalPrice(Rest, Remaining),
+    TotalPrice is Price + Remaining.
+
+% 6
+isBoycott(Name) :-
+   boycott_company(Name, _);
+   (
+     item(Name, Company, _),
+     boycott_company(Company, _)
+   ).
+
+% 9
+replaceBoycottItemsFromAnOrder(UserName, OrderId, NewList) :-
+    getItemsInOrderById(UserName, OrderId, Items),
+    replaceItems(Items, NewList).
+
+replaceItems([], []).
+replaceItems([Item|Rest], [NewItem|NewRest]) :-
+    (
+    isBoycott(Item) ->
+        (
+        alternative(Item, Alternative) ->
+            NewItem = Alternative
+        ;
+            NewItem = none
+        )
+    ;
+        NewItem = Item
+    ),
+    replaceItems(Rest, NewRest).
+
+% 10
+calcPriceAfterReplacingBoycottItemsFromAnOrder(UserName, OrderId, NewList, TotalPrice) :-
+    replaceBoycottItemsFromAnOrder(UserName, OrderId, NewList),
+    calcTotalPrice(NewList, TotalPrice).
+
